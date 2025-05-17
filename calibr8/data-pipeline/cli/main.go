4ce7460e80cab82078c61689ad89b5f4cb34b621
Kunal L <kunallimaye@gmail.com>
2025-05-18 06:08:37 +1000

    Filtering works locally

--- calibr8/data-pipeline/cli/main.go
+++ calibr8/data-pipeline/cli/main.go
@@ -3,18 +3,9 @@ package main
 import (
 	"github.com/dcentraldev/scratch/calibr8/data-pipeline/pkg/config"
 	"github.com/dcentraldev/scratch/calibr8/data-pipeline/pkg/pipeline"
-	"github.com/finiteloopme/goutils/pkg/log"
 )
 
-var targetTokens = map[string]string{
-	"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v": "USDC",
-	"So11111111111111111111111111111111111111112":  "SOL", // Wrapped SOL, native SOL handled by lamport transfers
-	"JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN":  "JUP",
-	"DRIFT6t7MAcxgfaX6B2sFfKCW5x8tUgtr2a3F8T8sZyh": "DRIFT",
-}
-
 func main() {
-	log.Info("Hello World")
 	cfg := config.GetConfig()
 	pipeline.Run(cfg)
 }
--- calibr8/data-pipeline/pkg/pipeline/pipeline.go
+++ calibr8/data-pipeline/pkg/pipeline/pipeline.go
@@ -2,11 +2,13 @@ package pipeline
 
 import (
 	"context"
+	"reflect"
 
 	"github.com/apache/beam/sdks/v2/go/pkg/beam"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/log"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/register"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/x/beamx"
+	"github.com/apache/beam/sdks/v2/go/pkg/beam/x/debug"
 	"github.com/dcentraldev/scratch/calibr8/data-pipeline/pkg/config"
 	"github.com/dcentraldev/scratch/calibr8/data-pipeline/pkg/transform"
 )
@@ -24,13 +26,13 @@ func ConstructPipeline(pipeline *beam.Pipeline, s beam.Scope, cfg *config.Config
 	impulse := beam.Impulse(s)
 
 	//beam.ParDo0(s.Scope("LogInfo"), Temp, impulse)
-	solClientFn, err := transform.NewChainClient(
-		cfg.HeliusGRPC,
-		cfg.HeliusAPIKey)
+	solClientFn, err := transform.NewChainClient(cfg)
 	if err != nil {
 		log.Fatalf(ctx, "Failed to create Solana client: %v", err)
 	}
-	beam.ParDo(s.Scope("SubscribeSol"), solClientFn, impulse)
+	logs := beam.ParDo(s.Scope("SubscribeSol"), solClientFn, impulse)
+	debug.Printf(s.Scope("EventInfo"), "Recevied event: %v", logs)
+	// beam.ParDo0(s.Scope("LogInfo"), LogInfo, logs)
 
 	beamx.Run(context.Background(), pipeline)
 }
@@ -40,6 +42,12 @@ func Temp(c []uint8) {
 
 }
 
+func LogInfo(v any) {
+	log.Infof(context.Background(), "Value %v", v)
+}
+
 func init() {
+	beam.RegisterType(reflect.TypeOf((*config.Config)(nil)))
 	register.Function1x0(Temp)
+	register.Function1x0(LogInfo)
 }
